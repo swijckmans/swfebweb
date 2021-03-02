1744c8b2aefb6fc327847e3b0ba5bfbd9bd487bd
SimonWijckmans <61318055+swijckmans@users.noreply.github.com>
2021-03-02 17:01:12 +0000

    adde file manually because I did a super dumb thing

---
+++ public/script.js
@@ -0,0 +1,15 @@
+window.addEventListener("load", function () {
+    const loader = document.querySelector(".loader");
+    console.log(loader);
+    loader.className += " hidden";
+
+});
+
+function show_menu() {
+    var x = document.getElementsByClassName("menuboxmobile")[0];
+    if (x.style.right === "0px") {
+        x.style.right = "-1000px";
+    } else {
+        x.style.right = "0px";
+    }
+}
